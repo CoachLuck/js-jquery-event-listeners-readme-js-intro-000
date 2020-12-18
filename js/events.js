f4b40667ec9dadca644143316799f8d57cdf1d2e
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 19:29:47 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 function pressIt() {
   $('$typing').on("keydown", function(key) {
     if key.which === 71 {
-      alert("'G was pressed'")
+      alert('G was pressed')
     }
   })
 }
