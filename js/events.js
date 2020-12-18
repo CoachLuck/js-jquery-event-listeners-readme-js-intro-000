b9fe6f53559bbd333eec6867d77893a1fc20954a
A.J. Romaniello <ajiellodev@gmail.com>
2020-12-18 19:39:17 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,10 @@
 //define functions here
 function getIt() {
-  $('p').on('click', function() {
-    alert("Hey!"))
-  }
+  $('p').on('click', alert("Hey!"));
 }
 
 function frameIt() {
-  $('img').on("load", $('img').addClass('tasty'))
+  $('img').on("load", $('img').addClass('tasty'));
 }
 
 function pressIt() {
